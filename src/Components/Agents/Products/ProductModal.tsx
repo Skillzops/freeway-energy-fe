79f3a52778b8ea7df109a580fd01cf6454b4c20f
Nastechy <Ibinichinasajoy@gmail.com>
2026-02-09 15:38:27 +0100

    modifications

--- src/Components/Agents/Products/ProductModal.tsx
+++ src/Components/Agents/Products/ProductModal.tsx
@@ -70,7 +70,7 @@ const ProductModal = ({
   refreshTable: KeyedMutator<any>;
 }) => {
   const fetchSingleProduct = useGetRequest(
-    `/v1/agents/product/${productID}`,
+    `/v1/products/${productID}`,
     false
   );
 
